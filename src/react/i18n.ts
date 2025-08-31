46e04ac67a560853ea311d8e2105aa3e1e59c117
reham-elsayed <rehamshipl445@gmail.com>
2025-08-31 05:09:00 +0300

    optional domControll

--- src/react/i18n.ts
+++ src/react/i18n.ts
@@ -3,7 +3,7 @@ import { initReactI18next } from 'react-i18next';
 import { AllLanguagesDictionary } from '../types';
 import { getDirection } from '../utils';
 
-export function setupI18n({ dictionaries, defaultLang }: { dictionaries: AllLanguagesDictionary, defaultLang: string }) {
+export function setupI18n({ dictionaries, defaultLang, controlDom = true }: { dictionaries: AllLanguagesDictionary, defaultLang: string, controlDom?: boolean }) {
     i18n.use(initReactI18next).init({
         resources: dictionaries,
         lng: defaultLang,
@@ -12,16 +12,18 @@ export function setupI18n({ dictionaries, defaultLang }: { dictionaries: AllLang
             escapeValue: false, // react already safes from xss
         },
     })
-    if (typeof window !== 'undefined') {
-        (window as any).i18n = i18n; // for debugging purposes
+    if (controlDom) {
+        if (typeof window !== 'undefined') {
+            (window as any).i18n = i18n; // for debugging purposes
 
-        i18n.on('languageChanged', (lang) => {
-            if (typeof window !== 'undefined') {
-                document.documentElement.lang = lang; // set the lang attribute on the html element
-                document.documentElement.dir = getDirection(lang); // set the dir attribute on the html element
-            }
-        })
-        i18n.emit('languageChanged', i18n.language); // trigger the event to set the initial lang and dir attributes
+            i18n.on('languageChanged', (lang) => {
+                if (typeof window !== 'undefined') {
+                    document.documentElement.lang = lang; // set the lang attribute on the html element
+                    document.documentElement.dir = getDirection(lang); // set the dir attribute on the html element
+                }
+            })
+            i18n.emit('languageChanged', i18n.language); // trigger the event to set the initial lang and dir attributes
+        }
     }
     return i18n;
 }
