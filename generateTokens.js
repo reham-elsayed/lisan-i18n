e8d6fc581b5d12c2b608f05a5564666982139211
reham-elsayed <rehamshipl445@gmail.com>
2025-09-14 20:06:29 +0300

    tokens sorted to en and ar

--- generateTokens.js
+++ generateTokens.js
@@ -9,12 +9,14 @@ function toCSSVars(obj, prefix = "") {
     let css = "";
     for (const key in obj) {
         const value = obj[key];
-        if (typeof value === "object") {
-
-            css += toCSSVars(value, `${prefix}-${key}`);
-        } else if (typeof value === "object" && "min" in value) {
+        if (typeof value === "object" && key === "font-size" && "min" in value) {
             // special case: responsive font-size object
             css += `-${prefix}-${key}: clamp(${value.min}, ${value.preferred}, ${value.max});\n`;
+            continue;
+        }
+        else if (typeof value === "object") {
+
+            css += toCSSVars(value, `${prefix}-${key}`);
         }
         else {
 
@@ -25,14 +27,14 @@ function toCSSVars(obj, prefix = "") {
 }
 
 // Build CSS output
-let css = `:root {\n${toCSSVars(tokens)}}
-\n
-[dir="rtl"] {
-  /* Example overrides — you can make this smarter later */
-  --spacing-sm: 0.75rem;
-}
-`;
 
+let css = ``
+if (tokens.en) {
+    css += `:root[dir="ltr"] {\n${toCSSVars(tokens.en)}\n}\n`;
+}
+if (tokens.ar) {
+    css += `:root[dir="rtl"] {\n${toCSSVars(tokens.ar)}\n}\n`;
+}
 // Write to a CSS file
 const outPath = path.join(__dirname, "dist", "tokens.css");
 fs.writeFileSync(outPath, css);
