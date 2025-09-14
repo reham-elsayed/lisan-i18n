d476257b14f4b0c083f634426bddcdee669ac118
reham-elsayed <rehamshipl445@gmail.com>
2025-09-14 20:08:13 +0300

    hook removed

--- src/react/index.ts
+++ src/react/index.ts
@@ -1,4 +1,3 @@
 export { setupI18n } from "./i18n";
 export { useTranslation, I18nextProvider } from "react-i18next";
 export { useDirection } from './hooks/useDirection';
-export { useFontScale } from './hooks/useFontScale';
